b705fac2d523a4513ab1b434b956f3373df82ea7
Ekky Kharismadhany <ekharisma@outlook.com>
2026-09-22 09:34:35 +0000

    save progress on terraform modules

---
+++ interact-with-terraform-modules/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+  default     = "qwiklabs-gcp-04-8d025fc51be5"
+}
+
+variable "name" {
+  description = "Name of the buckets to create."
+  type        = string
+  default     = "qwiklabs-gcp-04-8d025fc51be5"
+}
